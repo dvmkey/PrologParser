52cdc8a736bac68d60b13fea786741852d83a113
dvmkey <42848512+dvmkey@users.noreply.github.com>
2024-10-05 21:03:43 -0400

    Made a working ElegantParser.

---
+++ ElegantParser.pl
@@ -0,0 +1,33 @@
+parse_file(FileName) :-
+    open(FileName, read, Stream),
+    parse_lines(Stream),
+    close(Stream).
+
+parse_lines(Stream) :-
+    read_line_to_string(Stream, Line),
+    ( Line \= end_of_file ->
+        parse(Line),
+        parse_lines(Stream)
+    ; true
+    ).
+
+parse(String) :-
+	split_string(String, " ", ",", Atoms),
+    phrase(instruction(X), Atoms),
+	write(X).
+
+instruction([X, [Y|Z]]) --> arguement(Y), ["="], command(X), arguement(Z).
+instruction([X, Y]) --> command(X), arguement(Y).
+
+command([X]) --> [X], {is_command(X)}.
+
+arguement([]) --> [].
+arguement(X) --> [X].
+arguement([X|T]) --> [X], arguement(T).
+
+commands(["LOAD"]).
+is_command(String) :-
+    commands(CommandList),
+    member(String, CommandList).
+
+
