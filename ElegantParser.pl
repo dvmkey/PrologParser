037911f03f58d667e4326ac8e5a67cc566a9d4a6
dvmkey <42848512+dvmkey@users.noreply.github.com>
2024-10-05 23:26:39 -0400

    Commented ElegantParser.

--- ElegantParser.pl
+++ ElegantParser.pl
@@ -1,17 +1,24 @@
+% DCG for ARG = CMD ARGS
 instruction([X, [Y|Z]]) --> arguement(Y), ["="], command(X), arguement(Z).
+
+% DCG for CMD ARGS
 instruction([X, Y]) --> command(X), arguement(Y).
 
+% DCG for CMD
 command([X]) --> [X].
 
+%DCGs for Argument lists
 arguement([]) --> [].
 arguement(X) --> [X].
 arguement([X|T]) --> [X], arguement(T).
 
+% Run this command to parse the file.  Pass it the name of your AST.
 parse_file(FileName, AST) :-
     open(FileName, read, Stream),
     read_lines(Stream, AST),
     close(Stream).
 
+% A subcommand that parses lines and adds them to the AST
 read_lines(Stream, AST) :-
     read_line_to_string(Stream, Line),
     ( Line \= end_of_file ->
@@ -21,6 +28,7 @@ read_lines(Stream, AST) :-
     ; AST = []
     ).
 
+% The parse function responsible for parsing lines.
 parse(String, ParsedList) :-
 	split_string(String, " ", ",", Atoms),
     phrase(instruction(X), Atoms),
