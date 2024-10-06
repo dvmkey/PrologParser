a16b988ab618fe659dca4f379323aa4313b6b385
dvmkey <42848512+dvmkey@users.noreply.github.com>
2024-10-05 23:18:57 -0400

    Finished ElegantParser.  No comments.

--- ElegantParser.pl
+++ ElegantParser.pl
@@ -1,34 +1,28 @@
-parse_file(FileName) :-
-    open(FileName, read, Stream),
-    parse_lines(Stream),
-    close(Stream).
-
-parse_lines(Stream) :-
-    read_line_to_string(Stream, Line),
-    ( Line \= end_of_file ->
-        parse(Line),
-        parse_lines(Stream)
-    ; true
-    ).
-
-parse(String) :-
-	split_string(String, " ", ",", Atoms),
-    phrase(instruction(X), Atoms),
-	write(X).
-
 instruction([X, [Y|Z]]) --> arguement(Y), ["="], command(X), arguement(Z).
 instruction([X, Y]) --> command(X), arguement(Y).
 
-command([X]) --> [X], {is_command(X)}.
+command([X]) --> [X].
 
 arguement([]) --> [].
 arguement(X) --> [X].
 arguement([X|T]) --> [X], arguement(T).
 
-commands(["COPY", "LOAD", "STORE", "CBRANCH", "BRANCH", "BRANCHIND", "CALL", "CALLIND", "RETURN",
-"PIECE", "SUBPIECE", "INT2FLOAT", "FLOAT2FLOAT", "TRUNC", "POPCOUNT", "CALLOTHER"]).
-is_command(String) :-
-    commands(CommandList),
-    member(String, CommandList).
+parse_file(FileName, AST) :-
+    open(FileName, read, Stream),
+    read_lines(Stream, AST),
+    close(Stream).
 
+read_lines(Stream, AST) :-
+    read_line_to_string(Stream, Line),
+    ( Line \= end_of_file ->
+        parse(Line, ParsedList),
+        read_lines(Stream, RestList),
+        AST = [ParsedList | RestList]
+    ; AST = []
+    ).
+
+parse(String, ParsedList) :-
+	split_string(String, " ", ",", Atoms),
+    phrase(instruction(X), Atoms),
+    ParsedList = X.
 
